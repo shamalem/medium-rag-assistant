8aa4eddd8e9a4494cfe99b4b5b4c8d65aa8f290b
shamalem <sham.alem@campus.technion.ac.il>
2026-05-27 18:16:27 +0300

    Create stats.py

---
+++ api/stats.py
@@ -0,0 +1,19 @@
+from http.server import BaseHTTPRequestHandler
+import json
+
+CHUNK_SIZE = 512
+OVERLAP_RATIO = 0.2
+TOP_K = 7
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        response = {
+            "chunk_size": CHUNK_SIZE,
+            "overlap_ratio": OVERLAP_RATIO,
+            "top_k": TOP_K
+        }
+
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(response).encode("utf-8"))
