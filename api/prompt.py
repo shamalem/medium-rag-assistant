5a8147c46ede92a0a7fb61793b31755ca54aaf6e
shamalem <sham.alem@campus.technion.ac.il>
2026-06-07 12:39:07 +0300

    Update prompt.py

--- api/prompt.py
+++ api/prompt.py
@@ -6,7 +6,7 @@ from openai import OpenAI
 from pinecone import Pinecone
 
 
-TOP_K = 8
+TOP_K = 9
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 OPENAI_BASE_URL = os.environ.get("OPENAI_BASE_URL", "https://api.llmod.ai")
